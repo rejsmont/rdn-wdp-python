652b3abb8b3b0b54d08c6ebd7f11406694d350fb
Radoslaw Ejsmont <radoslaw@ejsmont.net>
2018-09-08 19:56:01 +0200

    Figure 3 generator

--- analysis/visualize.py
+++ analysis/visualize.py
@@ -115,10 +115,6 @@ gxmin = 0
 gxmax = 80
 gymin = -10
 gymax = 45
-# gxmin = round(input['cx'].min())
-# gxmax = round(input['cx'].max())
-# gymin = round(input['cy'].min())
-# gymax = round(input['cy'].max())
 
 cherry_min = input['mCherry'].min()
 cherry_max = input['mCherry'].mean() * 5
@@ -133,109 +129,61 @@ vmin = 0.1
 vmax = 15
 
 
-def plot_summary():
-    target_mean = input.groupby(['Gene', round(input['cy'])])['Venus'].mean()
-    target_max = input.groupby(['Gene', round(input['cy'])])['Venus'].quantile(0.99)
-    ato = input.groupby([round(input['cy'])])['mCherry'].mean()
-
-    genes = target_mean.index.get_level_values(0).unique()
-    fig = plt.figure(figsize=(30, 30))
-    count = len(genes)
-    sx = round(math.sqrt(count))
-    sy = math.ceil(count / sx)
-    sn = 1
-    for gene in genes:
-        ax = fig.add_subplot(sx, sy, sn)
-        ax.set_title(gene + ' expression profile')
-        div = make_axes_locatable(ax)
-        x = target_mean[gene].index
-        y = savgol_filter(target_mean[gene].values, 11, 3)
-        ax.plot(x, y, label='Target mean')
-        x = target_max[gene].index
-        y = savgol_filter(target_max[gene].values, 11, 3)
-        ax.plot(x, y, label='Target Q99')
-        x = ato.index
-        y = savgol_filter(ato.values, 11, 3)
-        ax.plot(x, y, label='Ato protein')
-        ax.set_xlim(gymin, gymax)
-        ax.set_ylim(0, 16)
-        sn = sn + 1
-    handles, labels = ax.get_legend_handles_labels()
-    fig.legend(handles, labels, loc='lower right')
-    plt.show()
-
-def plot_sample(sample):
-    print(sample)
-    data = input[input['Sample'] == sample]
-    plot_data(data, sample)
-
-
-def plot_target(target):
-    print(target)
-    data = input[input['Gene'] == target]
-    plot_data(data)
-
-
-def plot_image(ax, data, channel, projection='mean', title=None, norm=None, cmap=None):
-
+def plot_image(ax, data, channel, projection='mean', norm=None, cmap=None):
     xmin = round(data['cx'].min())
     xmax = round(data['cx'].max())
     ymin = round(data['cy'].min())
     ymax = round(data['cy'].max())
-
-    if title:
-        ax.set_title(title)
-
     img = ax.imshow(disc_matrix(data, channel, method=projection),
                     extent=[xmin, xmax, ymax, ymin], norm=norm, cmap=cmap, aspect='auto')
-
-    ax.set_aspect('equal')
     ax.set_facecolor('black')
     ax.set_xlim(gxmin, gxmax)
     ax.set_ylim(gymax, gymin)
     ax.xaxis.tick_top()
-
     return img, ax
 
 
-def plot_data(data, sample=None):
+def plot_profiles(ax, data, ticks):
+    target_mean = data.groupby([round(data['cy'])])['Venus'].mean()
+    x = target_mean.index
+    y = savgol_filter(target_mean.values, 11, 3)
+    ax.plot(x, y, label='Target mean')
+    target_max = data.groupby([round(data['cy'])])['Venus'].quantile(0.99)
+    x = target_max.index
+    y = savgol_filter(target_max.values, 11, 3)
+    ax.plot(x, y, label='Target Q99')
+    ato = input.groupby([round(input['cy'])])['mCherry'].mean()
+    x = ato.index
+    y = savgol_filter(ato.values, 11, 3)
+    ax.plot(x, y, label='Ato protein')
+    ax.set_xlim(gymin, gymax)
+    ax.set_yscale('log')
+    ax.set_ylim(0.1, 30)
+    ax.set_yticks(ticks)
+    ax.yaxis.set_major_formatter(fig_3c_major_formatter)
+    return ax
 
-    target = data['Gene'].unique()[0]
-    cherry = disc_matrix(data, 'mCherry', method='mean')
-    venus = disc_matrix(data, 'Venus', method='mean')
-    venmax = disc_matrix(data, 'Venus', method='max')
 
-    xmin = round(data['cx'].min())
-    xmax = round(data['cx'].max())
-    ymin = round(data['cy'].min())
-    ymax = round(data['cy'].max())
+def plot_legends(fig, gs, rows, columns, img0, img1, img2, handles, labels, ticks):
+    origin = rows * columns * 5
+    cax = fig.add_subplot(gs[origin + 1])
+    cbar = fig.colorbar(img0, cax=cax, orientation='horizontal', ticks=ticks,
+                        format=fig_3c_major_formatter, label='Mean target expression')
+    cbar.ax.margins(1.2, 0.2)
 
-    image = np.stack((
-        display_normalize(cherry, vmin, 1.5, log=False),
-        display_normalize(venus, vmin, 1.5, log=False),
-        display_normalize(venmax, vmin, vmax, log=True)), axis=2)
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    div = make_axes_locatable(ax)
+    cax = fig.add_subplot(gs[origin + 2])
+    cbar = fig.colorbar(img1, cax=cax, orientation='horizontal', ticks=ticks,
+                        format=fig_3c_major_formatter, label='Max target expression')
+    cbar.ax.margins(0.2, 0.2)
 
-    if sample:
-        ax.set_title(target + ' expression (' + sample + ')')
-        filename = target + '_' + sample + '.png'
-    else:
-        ax.set_title(target + ' expression')
-        filename = target + '.png'
+    cax = fig.add_subplot(gs[origin + 3])
+    cbar = fig.colorbar(img2, cax=cax, orientation='horizontal', ticks=ticks,
+                        format=fig_3c_major_formatter, label='Max target eccentricity')
+    cbar.ax.margins(0.2, 0.2)
 
-    img = ax.imshow(image, extent=[xmin, xmax, ymax, ymin])
-
-    ax.set_aspect('equal')
-    ax.set_facecolor('black')
-    ax.set_xlim(gxmin, gxmax)
-    ax.set_ylim(gymax, gymin)
-
-    if args.dir:
-        plt.savefig(os.path.join(dir, filename))
-    else:
-        plt.savefig(filename)
+    cax = fig.add_subplot(gs[origin + 4])
+    cax.set_axis_off()
+    cax.legend(handles, labels, ncol=2, loc='center', frameon=False)
 
 
 def genes_sorted(data):
@@ -250,8 +198,9 @@ def fig_3c_major_formatter(x, pos):
     return "%g" % (round(x * 10) / 10)
 
 
-def fig_3_row(fig, gs, gene, row, rows, ticks):
-    pos = (row - 1) * 5
+def fig_3_row(fig, gs, gene, index, rows, columns, ticks):
+    row = math.ceil(index / columns)
+    pos = (index - 1) * 5
     data = input[input['Gene'] == gene]
 
     ax = fig.add_subplot(gs[pos])
@@ -259,84 +208,41 @@ def fig_3_row(fig, gs, gene, row, rows, ticks):
     ax.text(0.5, 0.5, gene, horizontalalignment='center', verticalalignment='center', fontsize=24, rotation=90)
 
     norm = colors.LogNorm(vmin=0.1, vmax=30)
-    img0, ax = plot_image(fig.add_subplot(gs[pos + 1]), data, 'Venus', projection='mean', title=False, norm=norm, cmap='plasma')
+    img0, ax = plot_image(fig.add_subplot(gs[pos + 1]), data, 'Venus', projection='mean', norm=norm, cmap='plasma')
     ax.tick_params(bottom=True, top=True, labelbottom=(row == rows), labeltop=(row == 1),
                    left=True, right=False, labelleft=True, labelright=False)
 
-    img1, ax = plot_image(fig.add_subplot(gs[pos + 2]), data, 'Venus', projection='max', title=False, norm=norm, cmap='plasma')
+    img1, ax = plot_image(fig.add_subplot(gs[pos + 2]), data, 'Venus', projection='max', norm=norm, cmap='plasma')
     ax.tick_params(bottom=True, top=True, labelbottom=(row == rows), labeltop=(row == 1),
                    left=True, right=False, labelleft=False, labelright=False)
 
     norm = colors.LogNorm(vmin=0.1, vmax=25)
-    img2, ax = plot_image(fig.add_subplot(gs[pos + 3]), data, 'ext_Venus', projection='max', title=False, norm=norm, cmap='viridis')
+    img2, ax = plot_image(fig.add_subplot(gs[pos + 3]), data, 'ext_Venus', projection='max', norm=norm, cmap='viridis')
     ax.tick_params(bottom=True, top=True, labelbottom=(row == rows), labeltop=(row == 1),
                    left=True, right=False, labelleft=False, labelright=False)
 
-    ax = fig.add_subplot(gs[pos + 4])
-    target_mean = data.groupby([round(data['cy'])])['Venus'].mean()
-    x = target_mean.index
-    y = savgol_filter(target_mean.values, 11, 3)
-    ax.plot(x, y, label='Target mean')
-
-    target_max = data.groupby([round(data['cy'])])['Venus'].quantile(0.99)
-    x = target_max.index
-    y = savgol_filter(target_max.values, 11, 3)
-    ax.plot(x, y, label='Target Q99')
-
-    ato = input.groupby([round(input['cy'])])['mCherry'].mean()
-    x = ato.index
-    y = savgol_filter(ato.values, 11, 3)
-    ax.plot(x, y, label='Ato protein')
-
-    ax.set_xlim(gymin, gymax)
-    ax.set_yscale('log')
-    ax.set_ylim(0.1, 30)
-    ax.set_yticks(ticks)
-    ax.yaxis.set_major_formatter(fig_3c_major_formatter)
+    ax = plot_profiles(fig.add_subplot(gs[pos + 4]), data, ticks)
     ax.tick_params(bottom=True, top=True, labelbottom=(row == rows), labeltop=(row == 1),
                    left=False, right=True, labelleft=False, labelright=True)
-
     handles, labels = ax.get_legend_handles_labels()
+
     return img0, img1, img2, handles, labels
 
 
-def fig_3(data):
+def fig_3(data, columns):
     genes = genes_sorted(data)
-    #genes = ['ato', 'sens', 'Brd']
     e_series = [0.1, 0.22, 0.47, 1, 2.2, 4.7, 10, 22]
-    rows = len(genes)
-    fig = plt.figure(figsize=(32, math.ceil(rows / 2) * 2.65))
-    width_ratios = [1]
-    width_ratios.extend([16] * 4)
-    width_ratios.extend([1])
-    width_ratios.extend([16] * 4)
-    height_ratios = ([8] * math.ceil(rows / 2))
-    height_ratios.extend([1])
-    gs = gridspec.GridSpec(math.ceil(rows / 2) + 1, 10, width_ratios=width_ratios, height_ratios=height_ratios)
-    img0, img1, img2, handles, labels = None, None, None, None, None
+    rows = math.ceil(len(genes) / columns)
+    fig = plt.figure(figsize=(32, rows * 2.65))
+    width_ratios = [item for sub in [[1], [16] * 4] * columns for item in sub]
+    height_ratios = [item for sub in [[8] * rows, [1]] for item in sub]
+    gs = gridspec.GridSpec(rows + 1, 5 * columns, width_ratios=width_ratios, height_ratios=height_ratios)
+    legend_data = ()
     for index, gene in enumerate(genes):
         print(index, gene)
-        img0, img1, img2, handles, labels = fig_3_row(fig, gs, gene, index + 1, rows, e_series)
-
-    cax = fig.add_subplot(gs[rows * 5 + 1])
-    cbar = fig.colorbar(img0, cax=cax, orientation='horizontal', ticks=e_series,
-                        format=fig_3c_major_formatter, label='Mean target expression')
-    cbar.ax.margins(1.2, 0.2)
-
-    cax = fig.add_subplot(gs[rows * 5 + 2])
-    cbar = fig.colorbar(img1, cax=cax, orientation='horizontal', ticks=e_series,
-                        format=fig_3c_major_formatter, label='Max target expression')
-    cbar.ax.margins(0.2, 0.2)
-
-    cax = fig.add_subplot(gs[rows * 5 + 3])
-    cbar = fig.colorbar(img2, cax=cax, orientation='horizontal', ticks=e_series,
-                        format=fig_3c_major_formatter, label='Max target eccentricity')
-    cbar.ax.margins(0.2, 0.2)
-
-    cax = fig.add_subplot(gs[rows * 5 + 4])
-    cax.set_axis_off()
-    cax.legend(handles, labels, ncol=2, loc='center', frameon=False)
+        legend_data = fig_3_row(fig, gs, gene, index + 1, rows, columns, e_series)
+    plot_legends(fig, gs, rows, columns, *legend_data, e_series)
     plt.show()
 
 
-fig_3(input)
+fig_3(input, 2)
