98bae97bfd84109ad73b82636ca72eb379dff80e
Radoslaw Ejsmont <radoslaw@ejsmont.net>
2018-10-31 18:28:48 +0100

    clustering now in separate file

---
+++ analysis/clustering.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+
+from analysis.figures_ng import Figure, DiscData
+import argparse
+import logging
+import os
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.cluster.hierarchy import fcluster
+from scipy import stats
+import operator
+import hdbscan
+import hashlib
+
+
+class Clustering(Figure):
+
+    def __init__(self, data, method=None, metric='euclidean', k=6, n=5, r=1):
+        super().__init__(data)
+        self.cells = self.data.cells()[self.data.clean_mask() & self.data.furrow_mask()].dropna()
+        self.method = method
+        self.metric = metric
+        self.k = k
+        self.r = r
+        self.n = n
+        self.methods = [
+            'single',
+            'complete',
+            'average',
+            'weighted',
+            'centroid',
+            'median',
+            'ward',
+        ]
+        self.sample_sets = pd.DataFrame(columns=['Sample_{0}'.format(s) for s in range(0, self.n)])
+        self.centroids = pd.DataFrame(columns=['SampleSet', 'Cluster', 'cy', 'mCherry', 'ext_mCherry'])
+
+    def compute(self):
+        def h_cluster(x, method=None):
+            if method is None:
+                method = self.method
+
+        def get_samples(n, unique=False):
+            samples = self.cells[['Sample', 'Gene']].drop_duplicates().values.tolist()
+            selected = []
+            genes = []
+            while len(selected) < n:
+                if len(samples) == 0:
+                    break
+                index = np.random.randint(0, len(samples))
+                sample, gene = samples.pop(index)
+                if gene not in genes:
+                    genes.append(gene)
+                    selected.append(sample)
+                elif not unique:
+                    selected.append(sample)
+            return sorted(selected)
+
+        def cluster(samples=None, method=None, id=None):
+            cells = self.cells
+            if method is None:
+                method = self.method
+            if samples is None:
+                samples = sorted(cells['Sample'].unique().tolist())
+            if id is None:
+                id = hashlib.md5(str(samples).encode()).hexdigest()
+            filter = cells['Sample'].isin(samples)
+            x = stats.zscore(cells.loc[filter, ['cy', 'mCherry', 'ext_mCherry']].values, axis=0)
+            z = linkage(x, method)
+            cells.loc[filter, method + '_' + id] = fcluster(z, self.k, criterion='maxclust')
+            return z
+
+        for i in range(0, self.r):
+            samples = get_samples(self.n)
+            id = hashlib.md5(str(samples).encode()).hexdigest()
+            print(id, samples)
+            for method in self.methods:
+                print("\tComputing " + method)
+                try:
+                    z = cluster(samples, method, id)
+                except Exception as e:
+                    print("Computing " + method + " for dataset " + id + " failed: " + str(e))
+
+    def plot(self, outdir):
+
+        def sorted_legend(handles, labels=None):
+            if labels is None:
+                handles, labels = handles
+            hl = sorted(zip(handles, labels), key=operator.itemgetter(1))
+            handles2, labels2 = zip(*hl)
+            labels3 = [l.replace(' 0', ' ') for l in labels2]
+            return handles2, labels3
+
+        def h_cluster_dendrogram(z, ax, c_colors):
+            dd = dendrogram(
+                z,
+                truncate_mode='lastp',  # show only the last p merged clusters
+                p=self.k,               # show only the last p merged clusters
+                leaf_rotation=90.,      # rotates the x axis labels
+                leaf_font_size=8,       # font size for the x axis labels
+                show_contracted=True,   # to get a distribution impression in truncated branches
+                # max_d=p,              # plot distance cutoff line
+                ax=ax,
+                link_color_func=lambda c: 'black'
+            )
+            ax.set_xticklabels(['Cluster %d' % c for c in range(1, len(ax.get_xmajorticklabels()) + 1)])
+            x_lbls = ax.get_xmajorticklabels()
+            num = -1
+            for lbl in x_lbls:
+                num += 1
+                lbl.set_color(c_colors(num))
+            return dd
+
+        # fig = plt.figure(figsize=[10, 10])
+        # ax = fig.add_subplot(2, 2, 1)
+        # ax_xi = fig.add_subplot(2, 2, 2)
+        # ax_xy = fig.add_subplot(2, 2, 4)
+
+        # cells.loc[cells.groupby('Cluster')['Cluster'].transform('count').sort_values(ascending=False).index]
+
+        # clusters = cells.groupby('Cluster')['cx'].count().sort_values(ascending=False).index.tolist()
+        # c_colors = plt.cm.get_cmap("gist_rainbow", len(clusters))
+        # h_cluster_dendrogram(z, ax, c_colors)
+        #
+        # for cluster in clusters:
+        #     if cluster == 0:
+        #         continue
+        #     c_cells = cells[cells['Cluster'] == cluster]
+        #     c_count = c_cells['Cluster'].count()
+        #     label = "Cluster " + '%02d' % cluster + " (" + str(c_count) + " cells)"
+        #     ax_xi.scatter(c_cells['cy'], c_cells['mCherry'], c=[c_colors(cluster - 1)], label=label)
+        #     ax_xy.scatter(c_cells['cx'], c_cells['cy'], c=[c_colors(cluster - 1)], label=label)
+        #
+        # handles, labels = sorted_legend(ax_xy.get_legend_handles_labels())
+        # ax = fig.add_subplot(2, 2, 3)
+        # ax.set_axis_off()
+        # ax.legend(handles, labels, frameon=False, fontsize=15, loc='center')
+        #
+        # filename = id + '_' + method + '_' + str(self.k) + '.png'
+        # fig.savefig(os.path.join(outdir, filename))
+        # plt.close(fig)
+        #
+        # samples = cells['Sample'].unique().tolist()
+        # for sample in samples:
+        #     fig = plt.figure(figsize=[10, 10])
+        #     ax_xyi = fig.add_subplot(2, 2, 1)
+        #     ax_xi = fig.add_subplot(2, 2, 4)
+        #     ax_xy = fig.add_subplot(2, 2, 2)
+        #     s_cells = cells[cells['Sample'] == sample].sort_values('mCherry')
+        #     ax_xyi.scatter(s_cells['cx'], s_cells['cy'], c=s_cells['mCherry'])
+        #     for cluster in clusters:
+        #         c_cells = s_cells[s_cells['Cluster'] == cluster]
+        #         c_count = c_cells['Cluster'].count()
+        #         label = "Cluster " + '%02d' % cluster + " (" + str(c_count) + " cells)"
+        #         ax_xi.scatter(c_cells['cy'], c_cells['mCherry'], c=[c_colors(cluster - 1)], label=label)
+        #         ax_xy.scatter(c_cells['cx'], c_cells['cy'], c=[c_colors(cluster - 1)], label=label)
+        #     handles, labels = sorted_legend(ax_xy.get_legend_handles_labels())
+        #     ax = fig.add_subplot(2, 2, 3)
+        #     ax.set_axis_off()
+        #     ax.legend(handles, labels, frameon=False, fontsize=15, loc='center')
+        #     filename = id + '_' + method + '_' + str(self.k) + '_' + sample + '.png'
+        #     fig.savefig(os.path.join(outdir, filename))
+        #     plt.close(fig)
+
+
+parser = argparse.ArgumentParser(description='Plot all data.')
+parser.add_argument('--data', required=True)
+parser.add_argument('--log')
+parser.add_argument('--outdir')
+parser.add_argument('--clusters', default=6)
+parser.add_argument('--samples', default=5)
+parser.add_argument('--repeats', default=1)
+parser.add_argument('--reproducible', dest='reproducible', action='store_true')
+parser.add_argument('--not-reproducible', dest='reproducible', action='store_false')
+parser.set_defaults(reproducible=False)
+
+args = parser.parse_args()
+
+if args.log:
+    logging.basicConfig(level=args.log.upper())
+    logging.getLogger('PIL.Image').setLevel(logging.INFO)
+    logging.getLogger('matplotlib').setLevel(logging.INFO)
+    logging.getLogger('joblib').setLevel(logging.INFO)
+
+data = DiscData(args)
+
+# def run_process(option):
+#     figure = Figure_9a76(data, gene=gene, sample='all', method=option, k=7)
+#     try:
+#         figure.plot(args.outdir)
+#     except Exception as e:
+#         print("Plotting " + figure.metric + " failed: " + str(e))
+#
+#
+# if __name__ == '__main__':
+#     with Pool(2) as p:
+#         p.map(run_process, multiopt)
+
+print(args.reproducible)
+
+if args.reproducible:
+    np.random.seed(0)
+else:
+    np.random.seed()
+
+fig_9a76 = Clustering(data, k=args.clusters, n=args.samples, r=args.repeats)
+fig_9a76.compute()
--- analysis/figures_ng.py
+++ analysis/figures_ng.py
@@ -2,7 +2,6 @@
 
 import argparse
 import logging
-import os
 import numpy as np
 import pandas as pd
 import math
@@ -12,24 +11,8 @@ import matplotlib.ticker as ticker
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from matplotlib import colors
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from multiprocessing import cpu_count, Pool
-import random
-from scipy.cluster.hierarchy import dendrogram, linkage
-from scipy.cluster.hierarchy import cophenet
-from scipy.cluster.hierarchy import fcluster
-from scipy.spatial.distance import pdist
-from scipy.spatial.distance import squareform
 from scipy.signal import savgol_filter
-from scipy import stats
-import scipy.optimize
-from shapely.geometry import Polygon
-import seaborn as sns
-from sklearn import preprocessing
-import multiprocessing
-import operator
-import hdbscan
-import hashlib
+
 
 GX_MIN = 0
 GX_MAX = 80
@@ -340,6 +323,9 @@ class DiscData:
         self._dv_profiles = None
         self._matrices = None
         self.clean_up()
+        self._clean_mask = None
+        self._background_mask = None
+        self._furrow_mask = None
 
     def clean_up(self):
         # Remove artifact from sample ZBO7IH
@@ -356,6 +342,26 @@ class DiscData:
         bad_cells = self._cells[self._cells['Gene'] == 'CG9801-B'].index
         self._cells = self._cells.drop(bad_cells)
 
+    # Binary masks for identifying cell types #
+
+    def clean_mask(self):
+        if self._clean_mask is None:
+            self._clean_mask = self._cells['Gene'].isin(self.genes_clean())
+        return self._clean_mask
+
+    def furrow_mask(self):
+        if self._furrow_mask is None:
+            self._furrow_mask = (self._cells['cy'] >= -10) & (self._cells['cy'] <= 10)
+        return self._furrow_mask
+
+    def background_mask(self):
+        if self._background_mask is None:
+            self._background_mask = (self._cells['cy'] >= -10) & (self._cells['cy'] <= -5)
+        return self._background_mask
+
+
+    # Shortcuts to get cells (filtered) #
+
     def cells(self):
         return self._cells
 
@@ -366,7 +372,7 @@ class DiscData:
 
     def cells_background(self):
         if self._cells_background in None:
-            self._cells_background = data[(data['cy'] >= -10) & (data['cy'] <= -5)]
+            self._cells_background = self._cells[(self._cells['cy'] >= -10) & (self._cells['cy'] <= -5)]
         return self._cells_background
 
     def cells_ato(self):
@@ -624,251 +630,22 @@ class Figure_79eb(Figure_3d51):
         Figure.plot(self)
 
 
-class Figure_9a76(Figure):
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Plot all data.')
+    parser.add_argument('--data', required=True)
+    parser.add_argument('--log')
+    parser.add_argument('--outdir')
+    args = parser.parse_args()
 
-    def __init__(self, data, method=None, metric='euclidean', k=5, n=5, r=1):
-        super().__init__(data)
-        cells = self.data.cells_clean().dropna()
-        cells = cells[(cells['cy'] >= -10) & (cells['cy'] <= 10)]
-        self.cells = cells
-        self.method = method
-        self.metric = metric
-        self.k = k
-        self.r = r
-        self.n = n
-        self.methods = [
-            'single',
-            'complete',
-            'average',
-            'weighted',
-            'centroid',
-            'median',
-            'ward',
-        ]
+    if args.log:
+        logging.basicConfig(level=args.log.upper())
+        logging.getLogger('PIL.Image').setLevel(logging.INFO)
+        logging.getLogger('matplotlib').setLevel(logging.INFO)
+        logging.getLogger('joblib').setLevel(logging.INFO)
 
-    def compute(self):
-        pass
+    data = DiscData(args)
+    #fig_3d51 = Figure_3d51(data)
+    #fig_3d51.show()
+    #fig_76eb = Figure_79eb(data)
+    #fig_76eb.show()
 
-    def plot(self, outdir):
-
-        def sorted_legend(handles, labels=None):
-            if labels is None:
-                handles, labels = handles
-            hl = sorted(zip(handles, labels), key=operator.itemgetter(1))
-            handles2, labels2 = zip(*hl)
-            labels3 = [l.replace(' 0', ' ') for l in labels2]
-            return handles2, labels3
-
-        def h_cluster(x, data=None, method=None):
-            if data is None:
-                data = self.cells
-            if method is None:
-                method = self.method
-            # Cophenetic Correlation Coefficient
-            # single        0.6151533846171713
-            # complete      0.658116073716809
-            # average       0.7925722498155269
-            # weighted      0.671872242956842
-            # centroid      0.8076145946224655
-            # median        0.6922364609628868
-            # ward          0.5678365002555245
-            z = linkage(x, method)
-            k = self.k
-
-            cells = data.copy()
-            cells['cluster'] = fcluster(z, k, criterion='maxclust')
-            return z, cells.loc[cells.groupby('cluster')['cluster'].transform('count').sort_values(ascending=False).index]
-
-        def h_cluster_dendrogram(z, ax, c_colors):
-            dd = dendrogram(
-                z,
-                truncate_mode='lastp',  # show only the last p merged clusters
-                p=self.k,               # show only the last p merged clusters
-                leaf_rotation=90.,      # rotates the x axis labels
-                leaf_font_size=8,       # font size for the x axis labels
-                show_contracted=True,   # to get a distribution impression in truncated branches
-                # max_d=p,              # plot distance cutoff line
-                ax=ax,
-                link_color_func=lambda c: 'black'
-            )
-            ax.set_xticklabels(['Cluster %d' % c for c in range(1, len(ax.get_xmajorticklabels()) + 1)])
-            x_lbls = ax.get_xmajorticklabels()
-            num = -1
-            for lbl in x_lbls:
-                num += 1
-                lbl.set_color(c_colors(num))
-            return dd
-
-        def hdbscan_cluster(x, data=None):
-            if data is None:
-                data = self.cells
-            #clusterer = hdbscan.HDBSCAN(metric=self.metric, algorithm='best', min_cluster_size=200)
-            clusterer = hdbscan.HDBSCAN(metric=self.metric)
-            clusterer.fit(x)
-            labels = clusterer.labels_
-            cells = data.copy()
-            cells['cluster'] = labels + 2
-            return cells
-
-        def get_samples(n, unique=False):
-            samples = self.cells[['Sample', 'Gene']].drop_duplicates().values.tolist()
-            selected = []
-            genes = []
-            while len(selected) < n:
-                if len(samples) == 0:
-                    break
-                index = np.random.randint(0, len(samples))
-                sample, gene = samples.pop(index)
-                if gene not in genes:
-                    genes.append(gene)
-                    selected.append(sample)
-                elif not unique:
-                    selected.append(sample)
-
-            return selected
-
-        def dataid(cells):
-            return hashlib.md5(str(cells['Sample'].unique().tolist()).encode()).hexdigest()
-
-        def cluster(data, method=None, id=None):
-            if method is None:
-                method = self.method
-            if id is None:
-                id = dataid(data)
-
-            fig = plt.figure(figsize=[10, 10])
-            ax = fig.add_subplot(2, 2, 1)
-            ax_xi = fig.add_subplot(2, 2, 2)
-            ax_xy = fig.add_subplot(2, 2, 4)
-
-            x = stats.zscore(data[['cy', 'mCherry', 'ext_mCherry']].values, axis=0)
-            # x = self.cells[['cy', 'mCherry', 'ext_mCherry']].values
-            z, cells = h_cluster(x, data, method)
-            clusters = cells.groupby('cluster')['cx'].count().sort_values(ascending=False).index.tolist()
-            c_colors = plt.cm.get_cmap("gist_rainbow", len(clusters))
-            h_cluster_dendrogram(z, ax, c_colors)
-
-            for cluster in clusters:
-                if cluster == 0:
-                    continue
-                c_cells = cells[cells['cluster'] == cluster]
-                c_count = c_cells['cluster'].count()
-                label = "Cluster " + '%02d' % cluster + " (" + str(c_count) + " cells)"
-                ax_xi.scatter(c_cells['cy'], c_cells['mCherry'], c=[c_colors(cluster - 1)], label=label)
-                ax_xy.scatter(c_cells['cx'], c_cells['cy'], c=[c_colors(cluster - 1)], label=label)
-
-            handles, labels = sorted_legend(ax_xy.get_legend_handles_labels())
-            ax = fig.add_subplot(2, 2, 3)
-            ax.set_axis_off()
-            ax.legend(handles, labels, frameon=False, fontsize=15, loc='center')
-
-            filename = id + '_' + method + '_' + str(self.k) + '.png'
-            fig.savefig(os.path.join(outdir, filename))
-            plt.close(fig)
-
-            samples = cells['Sample'].unique().tolist()
-            for sample in samples:
-                fig = plt.figure(figsize=[10, 10])
-                ax_xyi = fig.add_subplot(2, 2, 1)
-                ax_xi = fig.add_subplot(2, 2, 4)
-                ax_xy = fig.add_subplot(2, 2, 2)
-                s_cells = cells[cells['Sample'] == sample].sort_values('mCherry')
-                ax_xyi.scatter(s_cells['cx'], s_cells['cy'], c=s_cells['mCherry'])
-                for cluster in clusters:
-                    c_cells = s_cells[s_cells['cluster'] == cluster]
-                    c_count = c_cells['cluster'].count()
-                    label = "Cluster " + '%02d' % cluster + " (" + str(c_count) + " cells)"
-                    ax_xi.scatter(c_cells['cy'], c_cells['mCherry'], c=[c_colors(cluster - 1)], label=label)
-                    ax_xy.scatter(c_cells['cx'], c_cells['cy'], c=[c_colors(cluster - 1)], label=label)
-                handles, labels = sorted_legend(ax_xy.get_legend_handles_labels())
-                ax = fig.add_subplot(2, 2, 3)
-                ax.set_axis_off()
-                ax.legend(handles, labels, frameon=False, fontsize=15, loc='center')
-                filename = id + '_' + method + '_' + str(self.k) + '_' + sample + '.png'
-                fig.savefig(os.path.join(outdir, filename))
-                plt.close(fig)
-
-        for i in range(0, self.r):
-            samples = get_samples(self.n)
-            cells = self.cells[self.cells['Sample'].isin(samples)]
-            id = dataid(cells)
-            print(id, samples)
-            for method in self.methods:
-                print("Computing " + method)
-                try:
-                    cluster(cells, method, id)
-                except Exception as e:
-                    print("Computing " + method + " for dataset " + id + " failed: " + str(e))
-
-
-parser = argparse.ArgumentParser(description='Plot all data.')
-parser.add_argument('--data', required=True)
-parser.add_argument('--log')
-parser.add_argument('--outdir')
-args = parser.parse_args()
-
-if args.log:
-    logging.basicConfig(level=args.log.upper())
-    logging.getLogger('PIL.Image').setLevel(logging.INFO)
-    logging.getLogger('matplotlib').setLevel(logging.INFO)
-    logging.getLogger('joblib').setLevel(logging.INFO)
-
-data = DiscData(args)
-#fig_3d51 = Figure_3d51(data)
-#fig_3d51.show()
-#fig_76eb = Figure_79eb(data)
-#fig_76eb.show()
-
-methods = [
-    'single',
-    'complete',
-    'average',
-    'weighted',
-    'centroid',
-    'median',
-    'ward',
-]
-
-# multiopt = [
-#     'braycurtis',
-#     'canberra',
-#     'chebyshev',
-#     'cityblock',
-#     'dice',
-#     'euclidean',
-#     'hamming',
-#     # 'haversine', # Haversine distance only valid in 2 dimensions
-#     'infinity',
-#     'jaccard',
-#     'kulsinski',
-#     'l1',
-#     'l2',
-#      # 'mahalanobis', # Must provide either V or VI for Mahalanobis distance
-#     'manhattan',
-#     'matching',
-#     # 'minkowski', # Minkowski metric given but no p value supplied!
-#     'p',
-#     # 'pyfunc', # pyfunc failed: __init__() takes exactly 1 positional argument (0 given)
-#     'rogerstanimoto',
-#     'russellrao',
-#     'seuclidean', # seuclidean failed: __init__() takes exactly 1 positional argument (0 given)
-#     #'sokalmichener',
-#     #'sokalsneath',
-#     # 'wminkowski' # wminkowski failed: __init__() takes exactly 2 positional arguments (0 given)
-# ]
-
-# def run_process(option):
-#     figure = Figure_9a76(data, gene=gene, sample='all', method=option, k=7)
-#     try:
-#         figure.plot(args.outdir)
-#     except Exception as e:
-#         print("Plotting " + figure.metric + " failed: " + str(e))
-#
-#
-# if __name__ == '__main__':
-#     with Pool(2) as p:
-#         p.map(run_process, multiopt)
-
-np.random.seed(0)
-fig_9a76 = Figure_9a76(data, k=6, n=10, r=1)
-fig_9a76.plot(args.outdir)
