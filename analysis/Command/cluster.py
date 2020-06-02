c94895d1de62ec63d5c7e6e9076ef50a224b7dcd
Radoslaw Ejsmont <radoslaw@ejsmont.net>
2020-06-02 16:43:29 +0200

    Playing with module paths

--- analysis/Command/cluster.py
+++ analysis/Command/cluster.py
@@ -2,12 +2,14 @@
 
 import argparse
 import logging
+import sys
+sys.path.append('../')
 
-from ..CellModels.Cells.Filters import Masks
-from ..CellModels.Cells.IO import CellReader
-from ..CellModels.Clustering.Compute import Clustering
-from ..CellModels.Clustering.Data import ClusteringConfig
-from ..CellModels.Clustering.IO import ClusteringResultsWriter
+from CellModels.Cells.Filters import Masks
+from CellModels.Cells.IO import CellReader
+from CellModels.Clustering.Compute import Clustering
+from CellModels.Clustering.Data import ClusteringConfig
+from CellModels.Clustering.IO import ClusteringResultsWriter
 
 
 def run(a):
